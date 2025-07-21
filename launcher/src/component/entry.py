4d3f39378812320aa62d5f85165b08bb79735cf3
justcarlux <manuelitojuan691@gmail.com>
2025-07-21 02:04:40 -0400

    feat(launcher): :sparkles: game thumbnails

--- launcher/src/component/entry.py
+++ launcher/src/component/entry.py
@@ -2,9 +2,10 @@ import pygame
 import os
 import sys
 import subprocess
-from util.asset_paths import font_path
+from util.asset_paths import font_path, thumbnail_path
 
 ROOT_GAMES_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+THUMBNAIL_OPACITY_STEP = 50
 
 class GameEntry:
     def __init__(self, index: int, name: str, entry_file: str, coords: tuple[int, int], surface: pygame.Surface):
@@ -22,13 +23,37 @@ class GameEntry:
         self.line_1_rect = self.line_1.get_rect(center=self.rect.center).move(0, -23)
         self.line_2 = self.font.render(name, True, self.text_color)
         self.line_2_rect = self.line_2.get_rect(center=self.rect.center).move(0, 23)
+        self.thumbnail = pygame.image.load(thumbnail_path(f"{index + 1}.png"))
+        self.thumbnail_pos = (
+            (surface.get_width() - self.thumbnail.get_width()) / 2,
+            (surface.get_height() - self.thumbnail.get_height()) / 2,
+        )
+        self.__thumbnail_opacity = 0
         
     def draw(self):
         self.hovered = self.rect.collidepoint(pygame.mouse.get_pos())
         pygame.draw.rect(self.surface, self.hover_color if self.hovered else self.color, self.rect, border_radius=12)
         self.surface.blit(self.line_1, self.line_1_rect)
         self.surface.blit(self.line_2, self.line_2_rect)
+
+        if (self.hovered):
+            self.__increment_thumbnail_opacity()
+        else:
+            self.__decrement_thumbnail_opacity()
         
+        self.thumbnail.set_alpha(self.__thumbnail_opacity)
+        self.surface.blit(self.thumbnail, self.thumbnail_pos)
+
+    def __increment_thumbnail_opacity(self):
+        self.__thumbnail_opacity += THUMBNAIL_OPACITY_STEP
+        if (self.__thumbnail_opacity > 255):
+            self.__thumbnail_opacity = 255
+
+    def __decrement_thumbnail_opacity(self):
+        self.__thumbnail_opacity -= THUMBNAIL_OPACITY_STEP
+        if (self.__thumbnail_opacity < 0):
+            self.__thumbnail_opacity = 0
+
     def on_click(self):
         working_dir = os.path.dirname(self.full_game_path)
         subprocess.Popen(
--- launcher/src/util/asset_paths.py
+++ launcher/src/util/asset_paths.py
@@ -5,6 +5,9 @@ ASSETS_ROOT = os.path.normpath(os.path.join(os.path.dirname(os.path.dirname(os.p
 def image_path(name: str):
     return os.path.join(ASSETS_ROOT, "images", name)
 
+def thumbnail_path(name: str):
+    return os.path.join(ASSETS_ROOT, "images", "thumbnails", name)
+
 def font_path(name: str):
     return os.path.join(ASSETS_ROOT, "fonts", name)
 
